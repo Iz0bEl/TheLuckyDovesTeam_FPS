c95279bf1a3f5e4fc117edb761f5ddc9a79b6fff
Jkeith24 <94270796+Jkeith24@users.noreply.github.com>
2022-12-04 09:01:07 -0500

    Update bullet.cs
    added the base functionality to bullet. Commented out the lines for the gameManager since it is not created

--- Assets/Scripts/Bullets.cs
+++ Assets/Scripts/Bullets.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int timer;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, timer);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            //waiting for gameManager to be setup
+            //gameManager.instance.playerScript.takeDamage(damage);
+        }
+        Destroy(gameObject);
     }
 }
