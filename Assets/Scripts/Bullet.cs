d346c3b7a619bf8c1091de60255b00cbfd0b3dab
Jkeith24 <94270796+Jkeith24@users.noreply.github.com>
2022-12-04 12:14:58 -0500

    Created player Prefab, edited playerController
    Created a player prefab and attached the player controller to it. I modified playerController script to shoot. I added some serialized fields for the shotgun and the sniper rifle guns

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullets : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
 
@@ -10,11 +10,14 @@ public class Bullets : MonoBehaviour
     [SerializeField] int speed;
     [SerializeField] int timer;
 
+    [Header("------ Shotgun Spread ------")]
+    [Range(1, 5)] [SerializeField] int shotgunBulletSpread;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = transform.forward * speed;
+        rb.velocity = transform.forward * speed;       
         Destroy(gameObject, timer);
     }
 
@@ -22,7 +25,7 @@ public class Bullets : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //waiting for gameManager to be setup
+            //+++++waiting for gameManager to be setup++++++
             //gameManager.instance.playerScript.takeDamage(damage);
         }
         Destroy(gameObject);
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -15,6 +15,23 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] int gravityValue;
     [SerializeField] int jumpsMax;
 
+    [Header("----- single Fire gun Stats -----")]
+    [SerializeField] int shootDamage;
+    [SerializeField] float shootRate;
+    [SerializeField] int shootDistance;
+
+    [Header("----- Shotgun Stats -----")]
+    [SerializeField][Range(0.1f,0.5f)] float shotGunDamagePerBullet;
+    [SerializeField] float ShotGunshootRate;
+    [SerializeField] int ShotGunRange;
+
+
+    [Header("----- Sniper Rifle Stats -----")]
+    [SerializeField] [Range(1.0f, 5.0f)] int SniperDamage;
+    [SerializeField] float SnipershootRate;
+    [SerializeField] int SniperRange;
+
+    bool isShooting;
 
     int jumpedTimes;
     private Vector3 playerVelocity;
@@ -30,6 +47,8 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         movement();
+       
+        StartCoroutine(shoot());
     }
 
 
@@ -54,5 +73,34 @@ public class PlayerControls : MonoBehaviour
 
         playerVelocity.y -= gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+    IEnumerator shoot()
+    {
+        if (!isShooting && Input.GetButton("Shoot"))
+        {
+            isShooting = true;
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, shootDistance))
+            {
+                if (hit.collider.GetComponent<IDamage>() != null)
+                {
+                    hit.collider.GetComponent<IDamage>().takeDamage(shootDamage);
+                }
+            }
+
+            Debug.Log("I shoot");
+            yield return new WaitForSeconds(shootRate);
+            isShooting = false;
+        }
+
+        //trying to emulate a shotgun by offsetting the raycast
+        //Vector3 shotgunDirection = transform.forward;
+        //shotgunDirection.x += Random.Range(-1, 1) * shotgunBulletSpread;
+        //shotgunDirection.y += Random.Range(-1, 1) * shotgunBulletSpread;
+        //shotgunDirection.z += Random.Range(-1, 1) * shotgunBulletSpread;
+
+        //rb.velocity = shotgunDirection
+
+
     }
 }
