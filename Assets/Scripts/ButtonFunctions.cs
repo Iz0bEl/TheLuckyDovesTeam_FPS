f6d39e4e6331ff6fa1415d11fe64b59e8877bd39
Iz0bEl <isobelhammonds@gmail.com>
2022-12-04 08:18:32 -0600

    Button functions (before game manager)

--- Assets/Scripts/ButtonFunctions.cs
+++ Assets/Scripts/ButtonFunctions.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonFunctions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ResumeButton()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RestartButton()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void RespawnPlayerButton()
+    {
+
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void AddJumpButton()
+    {
+
     }
 }
