2680eac6fae407ded8b40e234aa3a96aec6eafe3
RyanX123 <RKXiong@student.fullsail.edu>
2022-12-03 21:26:53 -0800

    Camera and Player controls

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+
+    [SerializeField] int sensHor;
+    [SerializeField] int sensVer;
+
+    [SerializeField] int lockVerMin;
+    [SerializeField] int lockVerMax;
+
+    [SerializeField] bool invertX;
+
+    float xRotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensVer;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensHor;
+
+        if (invertX)
+            xRotation += mouseY;
+        else
+            xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, lockVerMin, lockVerMax);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        transform.parent.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerControls : MonoBehaviour
 {
+    [Header("-----Components-----")]
+
+    [SerializeField] CharacterController controller;
+
+    [Header("-----Player Stats-----")]
+    [SerializeField] int HP;
+    [SerializeField] int playerSpeed;
+    [SerializeField] int jumpHeight;
+    [SerializeField] int gravityValue;
+    [SerializeField] int jumpsMax;
+
+
+    int jumpedTimes;
+    private Vector3 playerVelocity;
+    Vector3 move;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+    }
+
+
+    void movement()
+    {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            jumpedTimes = 0;
+            playerVelocity.y = 0f;
+        }
+
+        move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+
+        if (Input.GetButtonDown("Jump") && jumpedTimes < jumpsMax)
+        {
+            jumpedTimes++;
+            playerVelocity.y = jumpHeight;
+        }
+
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
