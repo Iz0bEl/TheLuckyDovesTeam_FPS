b8b800098db8cea3c4418ae577fd00a1909c7bc6
Iz0bEl <isobelhammonds@gmail.com>
2022-12-04 19:09:27 -0600

    Hopefully fixed pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -38,7 +38,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Cancel") && activeMenu == null)
+        if (Input.GetButton("Cancel") && activeMenu == null)
         {
             isPaused = !isPaused;
             activeMenu = pauseMenu;
