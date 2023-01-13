71737bdc1d977d7b9c3010ddc1690020de06d638
Iz0bEl <isobelhammonds@gmail.com>
2023-01-13 14:58:50 -0600

    Loading Screen script and Music script

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public GameObject LoadScreen;
+    public Image LoadBarFill;
+
+    public void LoadtheScene(int sceneID)
+    {
+        StartCoroutine(LoadSceneAsync(sceneID));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneID)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+
+            LoadBarFill.fillAmount = progressValue;
+
+            yield return null;
+        }
+    }
+}
